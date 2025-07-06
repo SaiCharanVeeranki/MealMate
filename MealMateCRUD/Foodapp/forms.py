9061b60c568ed6c5cd5ae0713868afcbcc8ff650
Sai Charan Veeranki <scv.learn@gmail.com>
2025-07-06 22:18:10 +0530

    Initial commit - Meal Mate Django CRUD project

---
+++ MealMateCRUD/Foodapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from Foodapp.models import Item
+
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['item_name','item_desc','price','item_image']
